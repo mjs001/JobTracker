9ad5b1ca4607b43afcda789edd8d57919b614f3f
miriam-sullivan <maljayd42@gmail.com>
2020-06-04 17:39:17 -0500

    added in media query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Everything from "./Components/Everything";
 import "./App.css";
+import {useMediaQuery} from react-responsive;
 export default function App() {
+  const phone = useMediaQuery({query: 'max-width: 500px'})
   return (
     <div className="App">
       <div className="components">
-        <Everything />
+  {phone && <Everything />}
       </div>
     </div>
   );
