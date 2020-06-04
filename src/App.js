a71d8d42d11127a1c092dfd01c2929f296ad4367
miriam-sullivan <maljayd42@gmail.com>
2020-06-04 17:44:00 -0500

    changed stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Everything from "./Components/Everything";
 import "./App.css";
-import {useMediaQuery} from react-responsive;
+import { useMediaQuery } from "react-responsive";
 export default function App() {
-  const phone = useMediaQuery({query: 'max-width: 500px'})
+  const phone = useMediaQuery({ query: "max-width: 500px" });
   return (
     <div className="App">
-      <div className="components">
-  {phone && <Everything />}
-      </div>
+      <div className="components">{phone && <Everything />}</div>
     </div>
   );
 }
